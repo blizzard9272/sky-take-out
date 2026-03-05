894555d1610a700500f7c15b6445ef50340a91ed
Blizzard <Hoisttheye@163.com>
2026-03-05 17:32:43 +0800

    完成分类相关接口开发

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,49 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    List<Category> getByType(Integer type);
+
+    /**
+     * 根据id更改分类信息
+     * @param category
+     */
+    void update(Category category);
+
+    /**
+     * 新增分类
+     * @param category
+     */
+    @Insert("insert into category" +
+            "(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+            "values" +
+            "(#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    void insert(Category category);
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    void deleteById(Long id);
+}
